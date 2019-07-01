6bac9da8951ed45c3a6de80533e7d728cbfb209d
Stephan Tittel <stephan.tittel@httc.de>
2019-07-01 12:04:18 +0200

    update

--- action-startExercise.py
+++ action-startExercise.py
@@ -6,6 +6,8 @@ from hermes_python.hermes import Hermes
 from hermes_python.ffi.utils import MqttOptions
 from hermes_python.ontology import *
 import io
+import random
+import os
 
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
